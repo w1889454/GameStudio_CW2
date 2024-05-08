0dadfb84d254334b695df9a12656db84ecebfb6a
Agyxagy <78908114+Agyxagy@users.noreply.github.com>
2024-05-08 19:27:27 +0100

    idek at this point

---
+++ GameStudio/Source/GameStudio/MyMenuHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MenuHUD.h"
+#include "MyMenuHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMESTUDIO_API AMyMenuHUD : public AMenuHUD
+{
+	GENERATED_BODY()
+	
+};
